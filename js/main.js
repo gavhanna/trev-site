1e9f6cd4c53ca6d77ee45680630f7d1e62443f47
gavhanna <gavhanna@gmail.com>
2016-06-12 23:17:24 +0100

    close dropdown on click outside

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ if (window.matchMedia("(min-width: 500px)").matches) {
     margins : 3
   });
 }
+
+$(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");
+    var _opened = $navbar.hasClass("in");
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+        $navbar.collapse('hide');
+    }
+});
